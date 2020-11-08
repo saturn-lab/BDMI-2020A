b056ee53ba36fb5da5fafc8a3a7ea6198c3d0e5e
Zhen Chen <zhenchen3419@users.noreply.github.com>
2020-11-08 10:52:32 +0800

    Create TensorFlow2-basic.cmd

---
+++ Schedule/Part1/WW9/TensorFlow2-basic.cmd
@@ -0,0 +1,10 @@
+| 时间段 |       内容         | 讲课 / 实践 | 分工  | 备注 |
+| :----- | :------------: | :---------: | :-----: | -----: |
+|    1   |  TensorFlow2 总体   |    讲课，实践       |  CZ   |   PPT   |
+|    2   |  即刻执行   |    实践     |  CZ   |  即刻执行： https://tensorflow.google.cn/guide/eager   |
+|    3   |  TensorFlow张量  |  讲课，实践    |    WX    |   TensorFlow张量：https://tensorflow.google.cn/guide/tensor    |
+|    4   |  TensorFlow变量  |    实践     |  GM   |  TensorFlow变量：https://tensorflow.google.cn/guide/variable     |
+|    5   |  自动微分    |    实践     |  ZY   |  自动微分：https://tensorflow.google.cn/guide/autodiff     |
+|    6   |  计算图  |  讲课，实践    |    WX    |  计算图：https://tensorflow.google.cn/guide/intro_to_graphs   |
+|    7   |  模型   |    实践     |  ZY   |  模型：https://tensorflow.google.cn/guide/intro_to_modules    |  
+|    8   |  循环训练  |  讲课，实践    |    GM    |  训练流程：https://tensorflow.google.cn/guide/basic_training_loops   |
